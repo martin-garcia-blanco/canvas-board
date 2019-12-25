fcf42c1aa6bebbbd44833777bcc2e3ac5a614c02
Martin Garcia Blanco <programadopormartin@gmail.com>
2019-12-25 21:02:11 +0100

    Restructure validators.js

--- canvas-utils/validator.js
+++ canvas-utils/validator.js
@@ -1,12 +1,9 @@
 const { ContentError } = require('./errors')
-
-
 const validator = {
 
     string(target){
         if( typeof a !== 'string' ) throw new ContentError(`${target} is not a string`)
     }
-
 }
 
 module.exports = validator
