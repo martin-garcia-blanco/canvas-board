91ba722b535aca79f0de848938cf0632240a26ac
Martin Garcia Blanco <programadopormartin@gmail.com>
2019-12-28 19:25:04 +0100

    create react-app wireframe

---
+++ canvas-app/src/components/App/index.js
@@ -0,0 +1,10 @@
+import React, { useState }  from 'react'
+import { Route, withRouter, Redirect } from 'react-router-dom'
+import './index.sass'
+
+export default withRouter(function () {
+
+
+    return <>
+    </>
+})
